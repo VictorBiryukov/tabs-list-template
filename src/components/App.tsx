4da5a77b1bd50247d535d98bf80149bbb2a22e54
sbt-biryukov-vv <bvvmail@gmail.com>
2022-02-08 11:01:00 +0300

    mvp orders_simple

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,47 +1,13 @@
 import React, { FC, useState, useRef } from 'react'
 import 'antd/dist/antd.css';
-import { Button, Form, Input, Layout, Modal } from 'antd';
-const { Content, Header } = Layout;
 import { AppProvider } from './AppProvider'
-
-
 
 export const App: FC = () => {
 
-    const [showLoginPage, setShowLoginPage] = useState<boolean>(false)
-    const [appAddress, setAppAddress] = useState<string>(localStorage.getItem("appAddress") as string)
     const initApolloClient = useRef<boolean>(true)
 
     return (
-        <Layout>
-            <Header>
-                <Button onClick={() => setShowLoginPage(true)}>
-                    Login page
-                </Button>
-                <Modal visible={showLoginPage}
-                    onCancel={() => setShowLoginPage(false)}
-                    onOk={() => {
-                        if (appAddress) {
-                            localStorage.setItem("appAddress", appAddress)
-                            setShowLoginPage(false)
-                            initApolloClient.current = true
-                        }
-                    }}
-                >
-                    <Form>
-                        <Form.Item>
-                            <Input placeholder="Service address"
-                                value={appAddress}
-                                onChange={e => setAppAddress(e.target.value)}
-                            />
-                        </Form.Item>
-                    </Form>
-                </Modal>
-            </Header>
-            <Content>
-                <AppProvider appAddress={appAddress}
-                    initApolloClient={initApolloClient} />
-            </Content>
-        </Layout>
+        <AppProvider appAddress={"http://localhsot:8081"}
+            initApolloClient={initApolloClient} />
     )
 }
--- src/components/AppTabs.tsx
+++ src/components/AppTabs.tsx
@@ -1,21 +1,104 @@
-import React, { FC } from 'react'
-import { Tabs } from 'antd'
+import React, { FC, useState, useRef } from 'react'
+import { Button, Form, Input, Layout, Modal, Tabs } from 'antd';
+
+import { useAppContext } from './AppContext'
+
+import { _CreateCustomerInput, CustomerAttributesFragment, useAddCustomerInfoMutation } from '../__generate/graphql-frontend'
 
 import { GoodTypeList } from './GoodTypeList'
 import { OrderList } from './OrderList'
+import { AllOrderList } from './AllOrderList'
+
 
 const { TabPane } = Tabs
+const { Header, Content } = Layout
+
+
+type InputParameters = Partial<_CreateCustomerInput>
+
+enum ShowForm {
+    None,
+    Update
+}
+
+function mapToInput(data: CustomerAttributesFragment | undefined): InputParameters {
+    const result = { ...data }
+    delete result.__typename
+    return result
+}
 
 export const AppTabs: FC = () => {
+
+    const appContext = useAppContext()
+
+    const [showForm, setShowForm] = useState<ShowForm>(ShowForm.None)
+    const [inputParameters, setInputParameters] = useState<InputParameters>({})
+
+    const [addCustomerInfoMutation] = useAddCustomerInfoMutation()
+
+
+    if (!inputParameters.id) {
+        addCustomerInfoMutation({variables: {customerInput: {id: appContext.userInfo!.email}}}).then(
+            result => setInputParameters(mapToInput(result.data!.packet!.updateOrCreateCustomer!.returning!))
+        )
+    }
+
     return (
-        <Tabs style={{ margin: "5px" }}>
-            <TabPane key="goods" tab="Goods">
-                <GoodTypeList></GoodTypeList>
-            </TabPane>
-            <TabPane key="orders" tab="Orders">
-                <OrderList></OrderList>
-            </TabPane>
-        </Tabs>
+
+        <Layout>
+            <Header>
+
+                <Button onClick={() => setShowForm(ShowForm.Update)}>
+                    {appContext.userInfo?.preferred_username + " personal data"}
+                </Button>
+                <Modal visible={showForm != ShowForm.None}
+                    onCancel={() => setShowForm(ShowForm.None)}
+                    onOk={() => {
+                        addCustomerInfoMutation({variables: {customerInput: Object.assign(inputParameters)}})
+                        setShowForm(ShowForm.None)
+                    }}
+                >
+                    <Form>
+                        <Form.Item>
+                            <Input placeholder="User name"
+                                value={inputParameters?.data?.name!}
+                                onChange={e => {
+                                    const input = { ...inputParameters }
+                                    input.data = { name: e.target.value, address: input.data?.address }
+                                    setInputParameters(input)
+                                }}
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input placeholder="User address"
+                                value={inputParameters?.data?.address!}
+                                onChange={e => {
+                                    const input = { ...inputParameters }
+                                    input.data = { name: input.data?.name, address: e.target.value }
+                                    setInputParameters(input)
+                                }}
+                            />
+                        </Form.Item>
+                    </Form>
+                </Modal>
+            </Header>
+            <Content>
+                <Tabs style={{ margin: "5px" }}>
+                    <TabPane key="goods" tab="Goods">
+                        <GoodTypeList></GoodTypeList>
+                    </TabPane>
+                    <TabPane key="orders" tab="My orders">
+                        <OrderList></OrderList>
+                    </TabPane>
+                    <TabPane key="all_orders" tab="All orders">
+                        <AllOrderList></AllOrderList>
+                    </TabPane>
+                </Tabs>
+            </Content>
+        </Layout>
+
+
+
     )
 
 }
